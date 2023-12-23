47e79a9eeb32c09a71dcef0f235d8a494aed099f
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2023-12-22 23:29:19 -0600

    Fixed AuthOptions type error in web application.

--- frontend/web-app/app/actions/authActions.ts
+++ frontend/web-app/app/actions/authActions.ts
@@ -1,30 +1,32 @@
 "use server";
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import {cookies, headers} from 'next/headers';
+import { cookies, headers } from "next/headers";
 import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
-export async function getSession(){
-    return await getServerSession(authOptions);
+import { authOptions } from "../api/auth/authOptions";
+export async function getSession() {
+  return await getServerSession(authOptions);
 }
-export async function getCurrentUser(){
-    try{
-        const session = await getSession();
-        if(!session) return null;
-        return session.user;
-    }
-    catch(error){
-        return null;
-    }
-    
+export async function getCurrentUser() {
+  try {
+    const session = await getSession();
+    if (!session) return null;
+    return session.user;
+  } catch (error) {
+    return null;
+  }
 }
 // This function is workaround to not being able to send a request object in the app router.
 // Not needed if we are working with NextAuthJS 5+.
-export async function getTokenWorkaround(){
-      const req = {
-        headers: Object.fromEntries(headers() as Headers),
-        cookies: Object.fromEntries(cookies().getAll().map(c=>[c.name, c.value])),
-      } as NextApiRequest
-      return await getToken({req}) 
+export async function getTokenWorkaround() {
+  const req = {
+    headers: Object.fromEntries(headers() as Headers),
+    cookies: Object.fromEntries(
+      cookies()
+        .getAll()
+        .map((c) => [c.name, c.value])
+    ),
+  } as NextApiRequest;
+  return await getToken({ req });
 }
