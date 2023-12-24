cbbac4793f7c554fcd7395f89f8d184e78e55139
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2023-12-24 01:28:54 -0600

    Behavior change when we click the login button

--- frontend/web-app/app/nav/LoginButton.tsx
+++ frontend/web-app/app/nav/LoginButton.tsx
@@ -1,12 +1,17 @@
 "use client";
-import { Button } from 'flowbite-react';
-import { signIn } from 'next-auth/react';
-import React from 'react'
+import { Button } from "flowbite-react";
+import { signIn } from "next-auth/react";
+import React from "react";
 
 export default function LoginButton() {
   return (
-    <Button outline onClick={()=> signIn('id-server', {callbackUrl: '/'})}> 
-    Login 
+    <Button
+      outline
+      onClick={() =>
+        signIn("id-server", { callbackUrl: "/" }, { prompt: "login" })
+      }
+    >
+      Login
     </Button>
-  )
+  );
 }
