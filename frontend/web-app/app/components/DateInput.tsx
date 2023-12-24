13004a480776eb53b8fcf8cddb0a3b1058ea2a88
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2023-12-24 15:59:07 -0600

    Datepicker CSS warning

--- frontend/web-app/app/components/DateInput.tsx
+++ frontend/web-app/app/components/DateInput.tsx
@@ -1,38 +1,37 @@
-import React from 'react'
-import { UseControllerProps, useController } from 'react-hook-form';
-import 'react-datepicker/dist/react-datepicker.css';
-import DatePicker, {ReactDatePickerProps} from 'react-datepicker';
+import React from "react";
+import { UseControllerProps, useController } from "react-hook-form";
+import DatePicker, { ReactDatePickerProps } from "react-datepicker";
 
 type Props = {
-    label: string;
-    type?:string;
-    showLabel?:string
-} & UseControllerProps & Partial<ReactDatePickerProps>
+  label: string;
+  type?: string;
+  showLabel?: string;
+} & UseControllerProps &
+  Partial<ReactDatePickerProps>;
 
 export default function DateInput(props: Props) {
-    // Give use controller hook access to our props.
-    const {fieldState, field} = useController({...props, defaultValue: ""});
+  // Give use controller hook access to our props.
+  const { fieldState, field } = useController({ ...props, defaultValue: "" });
 
   return (
-    <div className='block'>
-        <DatePicker
-            {...props}
-            {...field}
-            onChange={value =>field.onChange(value)}
-            selected={field.value}
-            placeholderText={props.label}
-            className={
-                `rounded-lg w-[100%] flex flex-col
-                ${fieldState.error ? 'bg-red-50 border-red-500 text-red-900' : 
-            'bg-green-50 border-green-500 text-green-900'}
-                `
-            }
-            />
-            {fieldState.error && (
-                <div className='text-red-500 text-sm'>
-                    {fieldState.error.message}
-                    </div>
-            )}
+    <div className="block">
+      <DatePicker
+        {...props}
+        {...field}
+        onChange={(value) => field.onChange(value)}
+        selected={field.value}
+        placeholderText={props.label}
+        className={`rounded-lg w-[100%] flex flex-col
+                ${
+                  fieldState.error
+                    ? "bg-red-50 border-red-500 text-red-900"
+                    : "bg-green-50 border-green-500 text-green-900"
+                }
+                `}
+      />
+      {fieldState.error && (
+        <div className="text-red-500 text-sm">{fieldState.error.message}</div>
+      )}
     </div>
-  )
+  );
 }
